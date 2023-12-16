75c44a6d37a82e6cac6ebf328cfb694316b981e2
Ivy <garfieldivymaitland@gmail.com>
2023-12-16 12:39:31 -0500

    More lessons in CLion

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
-#include <iostream>
+#include <iostream> // is a library
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "We did it" << std::endl;
+    std::cout << "The goal for today is to do more lessons in CLion" << std::endl;
     return 0;
 
 }
