7e08128c4980d22c3c82abe1b9c3f5bc875ad776
Ivy <garfieldivymaitland@gmail.com>
2023-12-16 17:56:54 -0500

    Learning about the stack, heap and pass by values

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream> // is a library
 #include <stdio.h>
 
+void setValue(int x){
+    printf("the value of x: %i\n", x);
+    x = 9999;
+    printf("the value of x: %i\n", x);
+    printf("x address: %p\n", (void*)&x);
+
+}
+
 int main() {
 
     std::cout << "Hello, World!" << std::endl;
@@ -11,11 +19,21 @@ int main() {
     int x = 5;
     int* px = &x;
 
-    printf("the value of x is %u\n", x);
-    printf("x address: %p\n",x);
-    printf("px stores: %p\n",px);
+    printf("the value of x is %u\n", x); // x is the integer value
+    printf("x's address: %p\n",&x); // &x is the address in memory of the integer value of x
+    printf("px stores: %p\n",px); // px is the pointer that stores the address in memory of the integer value x
+    printf("px's address: %p\n",&px); // &px is the address in memory of the pointer px
+    printf("px points to the value %i\n",*px); // *px points to the integer value of x
+
+    *px = 100; // this allows us to update the integer value of x
+
+    printf("px's address: %p, %p, %i, %u\n", &px, px, *px, x); // everything is same except the integer of x, x = 100
+
+    int a = 6;
+    setValue(a);
+    printf("a is: %d\n", a);
+
+    printf("a address: %p\n", (void*)&a);
 
     return 0;
-
-
 }
