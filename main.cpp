6f6bffeda382892774ffcbed57e606d73b0d3619
Ivy <garfieldivymaitland@gmail.com>
2023-12-31 12:49:44 -0500

    int y = 51

--- main.cpp
+++ main.cpp
@@ -17,7 +17,7 @@ int main() {
     std::cout << "Ready?" << std::endl;
 
     int x = 5;
-    int y = 49;
+    int y = 51;
     int* px = &x;
 
     printf("the value of x is %u\n", x); // x is the integer value
